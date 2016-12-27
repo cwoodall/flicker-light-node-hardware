79fdb3340a8e96999894a275c76b0a552b592980
Christopher J. Woodall <chris.j.woodall@gmail.com>
2016-12-27 15:18:41 -0500

    Cleaned up genbom script

--- scripts/genbom.py
+++ scripts/genbom.py
@@ -3,69 +3,79 @@
 # This script needs some improvement
 # (c) 2015 Productize <joost@productize.be>
 
-import sys, copy, collections, codecs
+import sys
+import copy
+import collections
+
 from bs4 import BeautifulSoup
+
+
+def stringify(obj):
+    if type(obj) is list:
+        item_str = ';'.join(c['Reference'])
+    else:
+        item_str = str(obj)
+    return '"{}"'.format(item_str)
+
 
 soup = BeautifulSoup(open(sys.argv[1]), 'lxml')
 
 date = soup.design.date.contents[0]
 
-comp = {}
-comp_nc = {}
+components = []
 
-standard_fields = ['Reference','Identifier','Package','Value','Tolerance','Voltage','Current','Power','PN']
+standard_fields = ['Reference',
+                   'Value',
+                   'Quantity',
+                   'Supplier',
+                   'Supplier Part Number',
+                   'Manufacturer',
+                   'Manufacturer Part Number']
+
 extra_fields = []
 
-i = 1
 for c in soup.components.find_all("comp"):
-  new_comp = collections.defaultdict(str)
-  new_comp['Reference'] = [c['ref']]
-  new_comp['Value'] = c.value.contents[0]
-  new_comp['Identifier'] = ''
-  new_comp['Package'] = ''
-  new_comp['Supplier'] = ''
-  new_comp['Supplier Part Number'] = ''
-  new_comp['Manufacturer'] = ''
-  new_comp['Manufacturer Part Number'] = ''
-  for f in c.find_all("field"):
-    new_comp[f['name']] = f.contents[0]
-    if f['name'] not in standard_fields:
-      if f['name'] not in extra_fields:
-        extra_fields.append(f['name'])
-  found = None
-  new_comp_no_ref = {}
-  for k in new_comp.keys():
-    if k != 'Reference':
-      new_comp_no_ref[k] = new_comp[k]
-  for c2 in comp.values():
-    c2_no_ref = {}
-    for k in c2.keys():
-      if k != 'Reference':
-        c2_no_ref[k] = c2[k]
-    if str(c2_no_ref) == str(new_comp_no_ref):
-      found = c2
-      c2['Reference'].append(c['ref'])
-      break
-  if found == None:
-    comp[i] = new_comp
-  i+=1
+    new_components = {}
+    new_components['Reference'] = [c['ref']]
+    new_components['Value'] = c.value.contents[0]
+    new_components['Quantity'] = 1
+    new_components['Identifier'] = ''
+    new_components['Package'] = ''
+    new_components['Supplier'] = ''
+    new_components['Supplier Part Number'] = ''
+    new_components['Manufacturer'] = ''
+    new_components['Manufacturer Part Number'] = ''
+    for f in c.find_all("field"):
+        new_components[f['name']] = f.contents[0]
+        if f['name'] not in standard_fields:
+            if f['name'] not in extra_fields:
+                extra_fields.append(f['name'])
+    found = None
+    new_comp_no_ref = {}
+    for k in new_components.keys():
+        if k not in ['Reference', 'Quantity']:
+            new_comp_no_ref[k] = new_components[k]
+    for c2 in components:
+        c2_no_ref = {}
+        for k in c2.keys():
+            if k not in ['Reference', 'Quantity']:
+                c2_no_ref[k] = c2[k]
+        print str(c2_no_ref)
+        print str(new_comp_no_ref)
+        if str(c2_no_ref) == str(new_comp_no_ref):
+            found = c2
+            c2['Reference'].append(c['ref'])
+            c2['Quantity'] += 1
+            break
+    if found is None:
+        components.append(new_components)
 
 
-with codecs.open(sys.argv[2],"w", encoding='utf-8') as f:
-  f.write("Reference,Quantity,Identifier,Package,Value,Supplier,Supplier Part Number,Manufacturer,Manufacturer Part Number")
-  for c in extra_fields:
-    f.write(","+c)
-  f.write('\r\n')
-  ordered = []
-  for k in comp.keys():
-    c = comp[k]
-    ref = ';'.join(c['Reference'])
-    l = [ref, str(len(c['Reference'])), c['Identifier'], c['Package'], c['Value'], c['Supplier'], c['Supplier Part Number'], c['Manufacturer'], c['Manufacturer Part Number']]
-    for y in extra_fields:
-      l.append(c[y])
-    #for y in c.keys():
-    #  if y not in standard_fields:
-    #    l.append
-    l = ['"'+x+'"' for x in l]
-    f.write(','.join(l))
-    f.write('\r\n')
+
+with open(sys.argv[2], "w") as f:
+    title_str = ','.join(standard_fields)
+    f.write(title_str + '\r\n')
+    for c in components:
+        l = [stringify(c[field]) for field in standard_fields]
+        f.write(','.join(l))
+        f.write('\r\n')
